7fe6456307cf7323307c75ea6ca1d3614069995b
Daniel E Fowler <def3332@gmail.com>
2020-12-10 23:02:34 -0600

    Update main.py
    added userdata

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import boto3
-import os
 import yaml
 
 
@@ -17,11 +16,13 @@ def run_ec2_from_file(filepath):
     response = ec2_client.create_key_pair(KeyName='ec2Key')
     with open('./my_key.pem', 'w') as file:
         file.write(response['key_material'])
-
+    ##user data for ssh
+    ud = build_userdata(server['users'])
     response = ec2_client.run_instances(
         ImageID=server['ami_type'],
         InstanceType=server['instance_type'],
         KeyName='ec2Key',
+        UserData=ud
     )
     instanceID = response['Instances'][0]['InstanceId']
     for v in server['volumes']:
@@ -42,6 +43,17 @@ def build_volumes(volume):
     )
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
         return response['VolumeId']
+
+def build_userdata(users):
+    ud = "#!/bin/bash " \
+         "echo "+ users[0]['ssh_key'] + " > /home/"+users[0]['login']+"/.ssh/authorized_keys" \
+         "chown "+ users[0]['login']+": /home/"+users[0]['login']+"/.ssh/authorized_keys" \
+         "chmod 0600 /home"+users[0]['login']+"/.ssh/authorized_keys" \
+         "echo "+ users[1]['ssh_key'] + " > /home/"+users[1]['login']+"/.ssh/authorized_keys" \
+         "chown "+ users[1]['login']+": /home/"+users[1]['login']+"/.ssh/authorized_keys" \
+         "chmod 0600 /home"+users[1]['login']+"/.ssh/authorized_keys"
+    return ud
+
 
 
 def run():
